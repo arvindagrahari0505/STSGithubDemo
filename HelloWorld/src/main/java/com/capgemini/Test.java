51f9e8ca2dc2e693c7991b153f39cc703a33d915
ARVAGRAH <ARVAGRAH@DIN51003425.corp.capgemini.com>
2019-03-15 12:19:14 +0530

    Second Change

--- HelloWorld/src/main/java/com/capgemini/Test.java
+++ HelloWorld/src/main/java/com/capgemini/Test.java
@@ -3,8 +3,8 @@ package com.capgemini;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World from gradle");
-
+		System.out.println("Hello World using github");
+		System.out.println("Hello World");
 	}
 
 }
